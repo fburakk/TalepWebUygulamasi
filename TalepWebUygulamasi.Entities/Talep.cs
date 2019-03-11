c96d86aa403803f2f97cb97c3e0951e637a362dc
fburakk <fburakbagci@gmail.com>
2019-03-11 22:33:34 +0300

    projects initiated, talep class added

---
+++ TalepWebUygulamasi.Entities/Talep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalepWebUygulamasi.Entities
+{
+    class Talep
+    {
+        public int TalepId { get; set; }
+        public string TalepKonusu { get; set; }
+        public string TalepAciklama { get; set; }
+    }
+}
